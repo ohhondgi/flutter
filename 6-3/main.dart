db43855ef75c769eb0f5d29f6e07cfbd5544cc4c
HONG GI OH <hong@HONGui-MacBookPro.local>
2021-02-04 23:58:35 +0900

    Container widgets

---
+++ 6-3/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('i am poor'),
+      ),
+      body: Center(child: Image(
+        image: AssetImage('images/coal.jpg'),
+      ),),
+    ),
+  ));
+}

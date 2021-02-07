9a5184d4631c2a98d25cb225bab1fb0225c0e180
오홍기 <5ghdrl@naver.com>
2021-02-07 23:44:49 +0900

    Flutter Layouts Challenge base

---
+++ 6-5/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100,
+                height: double.infinity,
+              ),
+              Container(
+                color: Colors.yellow,
+                width: 100,
+                height: 100,
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100,
+                height: double.infinity,
+              ),
+            ]
+          ),
+        )
+      ),
+    );
+  }
+}

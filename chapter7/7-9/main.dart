867d40f4da1f3bec8661dec36426bf4100520a39
오홍기 <5ghdrl@naver.com>
2021-02-26 23:45:09 +0900

    Randomising Dice with function

--- chapter7/7-9/main.dart
+++ chapter7/7-9/main.dart
@@ -24,31 +24,33 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+
+  setRamdomDiceNumber() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
           Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6)+1;
-                });
-              },
-              child: Image.asset('images/dice$leftDiceNumber.png'),
-            ),
-          ),
+              child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      setRamdomDiceNumber();
+                    });
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'))),
           Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  rightDiceNumber = Random().nextInt(6)+1;
-                });
-              },
-              child: Image.asset('images/dice$rightDiceNumber.png'),
-            ),
-          )
+              child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      setRamdomDiceNumber();
+                    });
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png')))
         ],
       ),
     );

6fae70ec73b16f350adcc32006ad935f1937b5b9
오홍기 <5ghdrl@naver.com>
2021-02-27 14:47:08 +0900

    challenge finish

--- chapter8/magic-8-ball-flutter/lib/main.dart
+++ chapter8/magic-8-ball-flutter/lib/main.dart
@@ -1,7 +1,45 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
+void main() =>
+    runApp(
       MaterialApp(
-        home: null,
+          home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.blue[900],
+              title: Center(
+                child: Text('Ask Me Anything'),
+              ),
+            ),
+            backgroundColor: Colors.blue,
+            body: magicBallPage(),
+          )),
+    );
+
+class magicBallPage extends StatefulWidget {
+  @override
+  _magicBallPageState createState() => _magicBallPageState();
+}
+
+class _magicBallPageState extends State<magicBallPage> {
+  int ballNum = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    ballNum = Random().nextInt(5)+1;
+                  });
+                },
+                child: Image.asset('images/ball$ballNum.png'),
+              )
+          ),
+        ],
       ),
     );
+  }
+}
